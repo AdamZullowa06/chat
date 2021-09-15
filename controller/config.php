c60274f7f2e7f80f05becac52cba9a82f333b35f
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-15 20:57:07 +0700

    fix error signup logic

--- controller/config.php
+++ controller/config.php
@@ -1,5 +1,10 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "chat");
-if ($conn) {
-    echo "Database connected" . mysqli_connect_error();
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "chat";
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+if (!$conn) {
+    echo "Database connection error" . mysqli_connect_error();
 }
