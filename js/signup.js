9e9035fd5214e1cd059145dccc542cc00d92f568
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-15 20:56:20 +0700

    fix error signup ajax

--- js/signup.js
+++ js/signup.js
@@ -1,14 +1,14 @@
 const form = document.querySelector(".signup form"),
 continueBtn = form.querySelector(".button input"),
-errorText = form.querySelector('.error-txt');
+errorText = form.querySelector(".error-txt");
 
-form.onsumbit = (e) => {
+form.onsubmit = (e) => {
     e.preventDefault();
 }
 
 continueBtn.onclick = () => {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "controller/signup.php", true);
+    xhr.open("POST", "controller/signup.php", false);
     xhr.onload = () => {
         if(xhr.readyState === XMLHttpRequest.DONE) {
             if(xhr.status === 200) {
@@ -16,8 +16,9 @@ continueBtn.onclick = () => {
                 if(data === "success") {
                     location.href = "user.php";
                 } else {
-                    errorText.textContent = data;
                     errorText.style.display = "block";
+                    errorText.textContent = data;
+                    error();
                 }
             }
         }
