d6c5ac6601cbef3fabe3f9ed051ed30265fe2cfc
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-15 20:56:47 +0700

    fix form signup

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if (isset($_SESSION['unique_id'])) {
-    header("location: user.php");
-}
+// if (isset($_SESSION['unique_id'])) {
+//     header("location : users.php");
+// }
 ?>
 
 <?php include_once "header.php"; ?>
@@ -11,21 +11,21 @@ if (isset($_SESSION['unique_id'])) {
     <div class="wrapper">
         <section class="form signup">
             <header>Chat.io</header>
-            <form action="#" enctype="multipart/form-data">
-                <div class="error-txt">This is an error message!</div>
+            <form action="#" method="post" enctype="multipart/form-data" autocomplete="off">
+                <div class="error-txt"></div>
                 <div class="name-details">
                     <div class="field input">
                         <label>First Name</label>
-                        <input type="text" name="fname" placeholder="First Name" required>
+                        <input type="text" name="fname" placeholder="First name" required>
                     </div>
                     <div class="field input">
                         <label>Last Name</label>
-                        <input type="text" name="lname" placeholder="Last Name" required>
+                        <input type="text" name="lname" placeholder="Last name" required>
                     </div>
                 </div>
                 <div class="field input">
                     <label>Email</label>
-                    <input type="email" name="email" placeholder="Enter your email" required>
+                    <input type="text" name="email" placeholder="Enter your email" required>
                 </div>
                 <div class="field input">
                     <label>Password</label>
@@ -34,13 +34,13 @@ if (isset($_SESSION['unique_id'])) {
                 </div>
                 <div class="field image">
                     <label>Select Image</label>
-                    <input type="file" name="image" required>
+                    <input type="file" accept="image/x-png,image/gif,image/jpeg,image/jpg" name="image" required>
                 </div>
                 <div class="field button">
-                    <input type="submit" value="Continue to chat">
+                    <input type="submit" name="submit" value="Continue to Chat">
                 </div>
             </form>
-            <div class="link">Already signed up? <a href="#">Login now</a></div>
+            <div class="link">Already signed up? <a href="login.php">Login now</a></div>
         </section>
     </div>
 
