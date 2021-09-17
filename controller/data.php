9739a01072b3275fb00119e9adb347586c9678cc
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-17 23:02:39 +0700

    making user page dynamic

---
+++ controller/data.php
@@ -0,0 +1,13 @@
+<?php 
+while ($row = mysqli_fetch_assoc($sql)) {
+    $output .= '<a href="#">
+                    <div class="content">
+                        <img src="controller/images/' . $row['img'] . '" alt="">
+                        <div class="details">
+                            <span>' . $row['fname'] . " " . $row['lname'] . '</span>
+                            <p>lorem ipsum dolor sit amet</p>
+                        </div>
+                    </div>
+                    <div class="status-dot"><i class="fas fa-circle"></i></div>
+                </a>';
+}
--- user.php
+++ user.php
@@ -1,22 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat.io | Send text to your friend</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
+<?php
+session_start();
+if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+}
+?>
+
+<?php include_once "header.php" ?>
+
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+                <?php
+                include_once "controller/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                if (mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+                }
+                ?>
                 <div class="content">
-                    <img src="portfolio.png" alt="">
+                    <img src="controller/images/<?php echo $row['img'] ?>" alt="">
                     <div class="details">
-                        <span>Adam</span>
-                        <p>Active now</p>
+                        <span><?php echo $row['fname'] . " " . $row['lname']; ?></span>
+                        <p><?php echo $row['status'] ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -27,99 +33,10 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="portfolio.png" alt="">
-                        <div class="details">
-                            <span>Ravi</span>
-                            <p>lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
             </div>
         </section>
-    </div>    
+    </div>
     <script src="js/users.js"></script>
 </body>
+
 </html>
