61431e5a825cee88d8dbcc184f1593c6baec9cae
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-18 23:06:44 +0700

    adding chat page

--- chat.php
+++ chat.php
@@ -1,22 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat.io | Send text to your friend</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
+<?php
+session_start();
+if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+}
+?>
+
+<?php include_once "header.php"; ?>
+
 <body>
     <div class="wrapper">
         <section class="chat-area">
             <header>
-                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                <img src="portfolio.png" alt="">
+                <?php
+                include_once "controller/config.php";
+                $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                if (mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+                }
+                ?>
+                <a href="user.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="controller/images/<?php echo $row['img'] ?>" alt="">
                 <div class="details">
-                    <span>Adam</span>
-                    <p>Active now</p>
+                    <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+                    <p><?php echo $row['status'] ?></p>
                 </div>
             </header>
             <div class="chat-box">
@@ -81,11 +88,17 @@
                     </div>
                 </div>
             </div>
-            <form action="#" class="typing-area">
-                <input type="text" placeholder="Type a message here...">
+            <form action="#" class="typing-area" autocomplete="off">
+                <input type="text" name="outgoing_id" value="<?php echo $_SESSION['unique_id']; ?>" hidden>
+                <input type="text" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+                <input type="text" name="message" class="input-field" placeholder="Type a message here...">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
         </section>
-    </div>    
+    </div>
+
+    <script src="js/chat.js"></script>
+
 </body>
+
 </html>
