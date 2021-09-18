9bdf4df011a5ee5704e0cf98d5eaebcd148d9624
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-18 23:07:01 +0700

    making chat area dynamic

--- controller/data.php
+++ controller/data.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 while ($row = mysqli_fetch_assoc($sql)) {
-    $output .= '<a href="#">
+    $output .= '<a href="chat.php?user_id=' . $row['unique_id'] . '">
                     <div class="content">
                         <img src="controller/images/' . $row['img'] . '" alt="">
                         <div class="details">
